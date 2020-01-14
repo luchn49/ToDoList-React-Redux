0d546b93aea984c531517c2b0f60098851f2d2e2
luchn49 <luchn49@gmail.com>
2020-01-14 16:49:30 +0700

    Updates

---
+++ precache-manifest.b7e0b7251bfbb913e69ad8367dc1bc91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074734e6facbeef7b1a06b92058f446b",
+    "url": "/ToDoList-React-Redux/index.html"
+  },
+  {
+    "revision": "c8d26a03fa415ae69350",
+    "url": "/ToDoList-React-Redux/static/css/main.671c3016.chunk.css"
+  },
+  {
+    "revision": "d5f31a462957fbcebb5b",
+    "url": "/ToDoList-React-Redux/static/js/2.98d5b904.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/ToDoList-React-Redux/static/js/2.98d5b904.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8d26a03fa415ae69350",
+    "url": "/ToDoList-React-Redux/static/js/main.1f3597ef.chunk.js"
+  },
+  {
+    "revision": "73919a50c4fd6fcb5838",
+    "url": "/ToDoList-React-Redux/static/js/runtime-main.da8cc839.js"
+  },
+  {
+    "revision": "226afc00d7697d99735b2a03a11de9dc",
+    "url": "/ToDoList-React-Redux/static/media/bg.226afc00.jpg"
+  }
+]);
